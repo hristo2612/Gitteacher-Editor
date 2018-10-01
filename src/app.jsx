e7145ca76043fdbb950ba99215103ef94bd7207e
theWeirdTeacher <chris.bogoev@gmail.com>
2018-10-01 20:46:08 +0300

    Add nifty cool link that downloads a text file

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import path from "path";
+import SearchBar from "./components/SearchBar";
 
 export default class App extends React.Component {
-
   componentWillMount() {
-    console.log('Mounted');
+    console.log("Mounted");
   }
 
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+        <a href={path.join(__dirname, "/someText.txt")} download={true}>
+          Fuck Exe
+        </a>
+      </div>
+    );
   }
 }
