511157ca2ec3863d1e878dfa6a89f4ae7c5e7921
hristo2612 <hristoapps@gmail.com>
2018-10-04 02:22:15 +0300

    Update app component

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Button, Divider } from "semantic-ui-react";
 import SearchBar from "./components/SearchBar";
 import OwnerCard from "./components/OwnerCard";
 import RepositoryCard from './components/RepositoryCard';
 import { getRepositoryStats } from "./endpoints";
 import request from "request";
+var Diff = require('react-diff');
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
       repoUrl: "",
-      repoName: "",
       avatarUrl: "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-      ownerName: "",
-      repositoryName: "...",
+      ownerName: "...",
+      repoName: "...",
+      disableCloneButton: true,
     };
     this.searchGithub = this.searchGithub.bind(this);
   }
   componentWillMount() {
     console.log("Mounted");
-    getRepositoryStats("https://github.com/hristo2612/Random-Cat")
+    getRepositoryStats("github.com/hristo2612/Gitteacher-Editor")
       .then(stats => {
         console.log(stats);
-        this.setState({ avatarUrl: stats.owner.avatar_url, ownerName: stats.owner.login, repositoryName: stats.name });
+        this.setState({ avatarUrl: stats.owner.avatar_url, ownerName: stats.owner.login, repoName: stats.name });
       })
       .catch(err => {
         console.log(err);
@@ -34,22 +35,28 @@ export default class App extends React.Component {
     getRepositoryStats(url)
     .then(stats => {
       console.log(stats);
-      this.setState({ avatarUrl: stats.owner.avatar_url, ownerName: stats.owner.login });
+      this.setState({ avatarUrl: stats.owner.avatar_url, ownerName: stats.owner.login, repoName: stats.name, disableCloneButton: false });
     })
     .catch(err => {
+      this.setState({ disableCloneButton: true });
       console.log(err);
     });
   }
 
   render() {
     return (
-      <div className="grid-container">
+      <div className="content">
         <h2>Gitteacher Editor!</h2>
         <p>Please enter your own repository url then hit GO.</p>
+        <Button style={{ float: 'right', margin: '5px' }} disabled={this.state.disableCloneButton} color="teal">Clone</Button>
         <SearchBar searchGithub={this.searchGithub} />
-        <OwnerCard avatarUrl={this.state.avatarUrl} name={this.state.ownerName}  />
-        <RepositoryCard name={this.state.repositoryName} />
+        <div className="grid-container">
+          <OwnerCard avatarUrl={this.state.avatarUrl} name={this.state.ownerName}  />
+          <RepositoryCard name={this.state.repoName} />
+        </div>
+        <Divider />
+        <Diff inputA="gogol" inputB="google" type="chars" />
       </div>
     );
-  }
+  } 
 }
