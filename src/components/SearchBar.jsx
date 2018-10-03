23b0b02e5598529080e55981f28188f136183ad2
theWeirdTeacher <chris.bogoev@gmail.com>
2018-10-03 21:57:19 +0300

    Update SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Input } from "semantic-ui-react";
 
 export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
   componentWillMount() {}
+
+  handleChange(event, data) {
+    this.props.searchGithub(data.value)
+  }
 
   render() {
     return (
       <div>
-        <input type={"text"} />
+        <Input list="languages" placeholder="Choose language..." onChange={this.handleChange} />
+        <datalist id="languages">
+          <option value="English" />
+          <option value="Chinese" />
+          <option value="Dutch" />
+        </datalist>
       </div>
     );
   }
